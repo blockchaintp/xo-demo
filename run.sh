e5c601cc8d7047609858cdfdde421666baa27f0f
Kai Davenport <kaiyadavenport@gmail.com>
2018-11-12 21:34:05 +0800

    allow the game to connect to any validator endpoint

--- run.sh
+++ run.sh
@@ -2,11 +2,13 @@
 
 set -e
 
-export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# read the environment to decide what the hostname and port
+# of the sawtooth validator is and rewrite the nginx.confg
+# using those values
 
-if [ -n "${MANUALRUN}" ]; then
-  echo "entering manual run mode"
-  sleep 10000000
-else
-  yarn run watch
-fi
+# by default we assume we are running inside the k8s pod
+# so the network is available on 127.0.0.1
+export REST_API_HOSTNAME=${REST_API_HOSTNAME:="127.0.0.1"}
+export REST_API_PORT=${REST_API_PORT:="8080"}
+cat /etc/nginx/nginx.template.conf | DOLLAR='$' envsubst > /etc/nginx/nginx.conf
+nginx -g 'daemon off;'
